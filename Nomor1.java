4aa4e2326333d26df7933af691e89144e2489a46
Agelino <144404858+Agelino@users.noreply.github.com>
2024-10-24 12:05:51 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,24 +1,24 @@
-import java.util.Scanner;
-
 public class Nomor1 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        
-    
-        System.out.print("Masukkan jumlah jam lembur: ");
-        int jamLembur = input.nextInt();
-        
+        int jamLembur = 12;  // Contoh nilai jam lembur
+
         double honorLembur;
-        
-        
+
+        // Perhitungan honor lembur
         if (jamLembur <= 16) {
             honorLembur = jamLembur * 10000;
         } else {
             honorLembur = (16 * 10000) + ((jamLembur - 16) * 15000);
         }
-        
+
         // Tampilkan hasil
         System.out.println("Total jam lembur: " + jamLembur);
         System.out.println("Honor lembur: " + honorLembur);
+
+         jamLembur =20;
+
+         System.out.println("Total jam lembur: " + jamLembur);
+        System.out.println("Honor lembur: " + honorLembur);
+
     }
 }
